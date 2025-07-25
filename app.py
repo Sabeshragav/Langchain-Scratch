83b587c370f56d3eae51807e6479a93a47aa9eb8
Sabeshragav <sabeshragav289@gmail.com>
2025-07-25 22:34:36 +0530

    Add .gitignore and initial app.py for environment setup and model invocation

---
+++ app.py
@@ -0,0 +1,16 @@
+import getpass
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+if not os.getenv("GROQ_API_KEY"):
+  os.environ["GROQ_API_KEY"] = getpass.getpass("Enter API key for Groq: ")
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("llama3-8b-8192", model_provider="groq")
+
+response = model.invoke("Whats the capital of Tamil Nadu")
+
+print(response)
